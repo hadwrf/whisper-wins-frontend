b9c6252d826acaced2b6e63aa19b8a2a20cade2b
Mehmet Harun Doganay <mehmet.doganay@check24.de>
2024-11-01 20:04:39 +0100

    Enable Vercel SpeedInsights metrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { inter } from '@/app/ui/fonts';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,6 +19,7 @@ export default function RootLayout({
         className={`${inter.className} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
