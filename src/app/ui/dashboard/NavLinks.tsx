41035bc7aa6ba8c6d8d457cc0c83ec9090819cae
Mehmet Harun Doganay <mehmet.doganay@check24.de>
2024-11-01 19:55:37 +0100

    Showing active links

--- src/app/ui/dashboard/NavLinks.tsx
+++ src/app/ui/dashboard/NavLinks.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
     HomeIcon,
     MegaphoneIcon,
@@ -5,6 +7,8 @@ import {
     ShoppingBagIcon
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -20,6 +24,8 @@ const links = [
 ];
 
 export default function NavLinks() {
+    const pathname = usePathname();
+
     return (
         <div className="flex gap-3">
             {links.map((link) => {
@@ -28,7 +34,12 @@ export default function NavLinks() {
                     <Link
                         key={link.name}
                         href={link.href}
-                        className="flex h-[48px] flex-none grow items-center justify-start gap-2 rounded-md bg-gray-50 p-2 px-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600"
+                        className={clsx(
+                            "flex h-[48px] flex-none grow items-center justify-start gap-2 rounded-md bg-gray-50 p-2 px-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600",
+                            {
+                                'bg-sky-100 text-blue-600': pathname === link.href,
+                            },
+                        )}
                     >
                         <LinkIcon className="w-6" />
                         <p>{link.name}</p>
