99b6152d4416fd35ebe9827d53c6781dcd240b12
Mehmet Harun Doganay <mehmet.doganay@check24.de>
2024-11-01 19:43:40 +0100

    Pages placeholders and App Routing
    To route the following pages you should direct to the following links:
    
    - Dashboard (Home): /dashboard
    - Explore: /dashboard/explore
    - My Bids: /dashboard/my-bids
    - Create Auction: /dashboard/create-auction
    
    The url has to match with the directory naming.
    It's called File-Based Routing.

---
+++ src/app/dashboard/create-auction/page.tsx
@@ -0,0 +1,7 @@
+const CreateAuction = () => {
+    return(
+        <h1>Create Auction</h1>
+    );
+}
+
+export default CreateAuction;
---
+++ src/app/dashboard/my-bids/page.tsx
@@ -0,0 +1,7 @@
+const MyBids = () => {
+    return(
+        <h1>My Bids</h1>
+    );
+}
+
+export default MyBids;
