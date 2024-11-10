1ebdcec0fa14dd895c7c47ae3a44e7eb43df87f0
Mehmet Harun Doganay <mehmet.doganay@check24.de>
2024-11-10 13:51:16 +0100

    Add shadcn toast component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
+import { inter } from '@/app/ui/fonts';
+import { Toaster } from '@/components/ui/toaster';
+import Providers from '@/lib/wagmi-providers';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import './globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { inter } from '@/app/ui/fonts';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/react';
-import Providers from '@/lib/wagmi-providers';
 import { headers } from 'next/headers';
 
 export const metadata: Metadata = {
@@ -27,6 +28,7 @@ export default function RootLayout({
         <Providers cookie={cookie}>{children}</Providers>
         <SpeedInsights />
         <Analytics />
+        <Toaster />
       </body>
     </html>
   );
