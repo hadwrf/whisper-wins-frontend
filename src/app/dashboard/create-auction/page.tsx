db0a770bc92df625f67ea48482848d48d97da798
Mehmet Harun Doganay <mehmet.doganay@check24.de>
2024-11-24 22:34:48 +0100

    Trigger build

--- src/app/dashboard/create-auction/page.tsx
+++ src/app/dashboard/create-auction/page.tsx
@@ -4,7 +4,7 @@ import { CreateAuctionForm } from './CreateAuctionForm';
 
 const CreateAuction = () => {
   return (
-    <div className='mx-auto mt-8 max-w-3xl'>
+    <div className='mx-auto mt-8 max-w-2xl'>
       <CreateAuctionForm />
     </div>
   );
