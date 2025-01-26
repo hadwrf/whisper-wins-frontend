fc3da9dfc3cffbe8299cc3e5dcdb0c68ae8c4dfb
Mehmet Harun Doganay <mehmet.doganay@check24.de>
2025-01-26 17:13:36 +0100

    Delete outdated and redundant abi

--- src/lib/abi/index.ts
+++ src/lib/abi/index.ts
@@ -1,21 +1,5 @@
-import { default as auctionContract } from './AuctionContract.json';
-import { default as factoryContract } from './FactoryContract.json';
-import { default as whisperBasicsContract } from './WhisperBasic.json';
-import { default as myFirstSuapp } from './MyFirstSuapp.json';
 import { default as basicAuctionContract } from './BasicAuctionContract.json';
-import { default as sealedAuctionContract } from './SealedAuction.json';
 import { default as sealedAuctionv2Contract } from './SealedAuctionv2.json';
-import { default as sealedAuctionv3Contract } from './SealedAuctionv3.json';
 import { default as erc721Abi } from './ERC721Abi.json';
 
-export {
-  auctionContract,
-  factoryContract,
-  whisperBasicsContract,
-  myFirstSuapp,
-  basicAuctionContract,
-  sealedAuctionContract,
-  sealedAuctionv2Contract,
-  sealedAuctionv3Contract,
-  erc721Abi,
-};
+export { basicAuctionContract, sealedAuctionv2Contract, erc721Abi };
